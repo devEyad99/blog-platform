a97be0f310b48628d9dfe7aa80d1f8e397d076a4
Eyad010 <eyadkhaled375@gmail.com>
2025-03-20 01:14:42 +0200

    Generated necessary NestJS modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, PostsModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
